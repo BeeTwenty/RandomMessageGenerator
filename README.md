0e3da2b5a20caafc72bcb8cda45880d493c6aeff
BeeTwenty <sindrebergee96@gmail.com>
2021-05-01 16:21:17 +0200

    Finished App

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ### What is this program?
 + It is a part of a course on CodeCademy
 + It is a program to Generate Random Messages!
+
+### To Run
++ Download Files
++ Run With $node app.js
---
+++ app.js
@@ -0,0 +1,5 @@
+let quotes = ["Act as if what you do makes a difference. It does.", "Success is not final, failure is not fatal: it is the courage to continue that counts.", "Never bend your head. Always hold it high. Look the world straight in the eye.",
+"What you get by achieving your goals is not as important as what you become by achieving your goals.","Believe you can and you're halfway there.","When you have a dream, you've got to grab it and never let go."];
+let randomNuber = Math.floor(Math.random() * 6);
+let message = quotes[randomNuber];
+console.log(message);
